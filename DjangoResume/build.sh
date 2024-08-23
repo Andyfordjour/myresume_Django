453622e040065ccb3f82aa3d52cee997e2cd4c45
Your Name <youremail@example.com>
2024-08-23 19:15:09 -0400

    update

--- DjangoResume/build.sh
+++ DjangoResume/build.sh
@@ -8,5 +8,4 @@ pip install -r DjangoResume/requirements.txt
 # Convert static asset files
 python DjangoResume/manage.py collectstatic --no-input
 
-# Apply any outstanding database migrations
-python DjangoResume/manage.py runserver
+

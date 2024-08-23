7880422343aa6f35e74932a9432b931ae2d5b78f
Your Name <youremail@example.com>
2024-08-23 19:09:40 -0400

    Updated

--- DjangoResume/build.sh
+++ DjangoResume/build.sh
@@ -6,7 +6,7 @@ set -o errexit
 pip install -r DjangoResume/requirements.txt
 
 # Convert static asset files
-python manage.py collectstatic --no-input
+python DjangoResume/manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python manage.py runserver
+python DjangoResume/manage.py runserver

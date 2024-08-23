e21ac2977e7130648798d2f577cacf5b24b41243
Your Name <youremail@example.com>
2024-08-23 19:54:11 -0400

    update

--- DjangoResume/build.sh
+++ DjangoResume/build.sh
@@ -5,6 +5,9 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r DjangoResume/requirements.txt
 
+
+export PYTHONPATH=/path/to/your/project:$PYTHONPATH
+
 # Convert static asset files
 python DjangoResume/manage.py collectstatic --no-input
 
